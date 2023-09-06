ef631798723f6959e6858c9c7ff23f741fe5c351
VladLore <vladiknikishin@gmail.com>
2023-09-06 14:57:33 +0300

    Решение HW3\task1

--- HW3/task1.py
+++ HW3/task1.py
@@ -1,4 +1,19 @@
-lst = [1, 2, 3, 4, 5, 1, 2]
-unique_list = set(lst)
-result = [item for item in unique_list]
-print(result)
+""""
+Дан список повторяющихся элементов. 
+Вернуть список с дублирующимися элементами. 
+В результирующем списке не должно быть дубликатов.
+"""
+
+l1 = [1, 2, 3, 4, 4, 5, 6, 5, 7]
+new_lst = []                   
+duplicate_lst = []                      
+
+for i in l1:
+    if i not in new_lst:
+        new_lst.append(i)
+    else:
+        duplicate_lst.append(i)
+
+print(l1)
+print(new_lst)
+print(duplicate_lst)
