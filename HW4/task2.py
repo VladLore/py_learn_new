741675fb856f195740b5f69538c9a332b3c1dbe0
VladLore <vladiknikishin@gmail.com>
2023-09-12 14:49:47 +0300

    Решение HW4\task2

--- HW4/task2.py
+++ HW4/task2.py
@@ -1,8 +1,6 @@
 stroka = ''' Напишите функцию, которая принимает строку текста. Сформируйте список с уникальными кодами Unicode каждого'''
 
 def uni_code(stroka):
-    """формирует список используемых символов по убываению"""
-
     return sorted([ord(i) for i in set(stroka)], reverse=True)
 
 print(uni_code(stroka))
